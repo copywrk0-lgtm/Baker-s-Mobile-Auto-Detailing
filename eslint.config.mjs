ef776eec197d34ff9308ee85aee7c1d8d1f4ebf8
copywrk0-lgtm <copywrk0@gmail.com>
2026-08-30 15:43:46 +0530

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,3 @@
-import { defineConfig } from 'eslint/config'
 import nextVitals from 'eslint-config-next/core-web-vitals.js'
 
-export default defineConfig([...nextVitals])
+export default [...nextVitals]
